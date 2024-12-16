3d0a0e7d123841049cd2a2109b63446eb708758c
ST3510 <ST3510@TE.local>
2024-12-16 12:27:47 +0600

    zd 4

--- lab1/lab1/lab1/users/views.py
+++ lab1/lab1/lab1/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+
 
 # Create your views here.
 def register_view(request):
@@ -24,3 +25,16 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", { "form": form })
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
+        if form.is_valid(): 
+            login(request, form.get_user())
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("posts:list")
+    else: 
+        form = AuthenticationForm()
