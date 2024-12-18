9e26d5c93adefcf68031a5dda6b2501c0465c9c4
Daniil <1111marmelad1111@gmail.com>
2024-12-18 22:26:43 +0600

    4

--- lab1/lab1/lab1/communities/urls.py
+++ lab1/lab1/lab1/communities/urls.py
@@ -6,5 +6,6 @@ app_name = 'communities'
 
 urlpatterns = [
     path('', views.communities, name="communities"),
+    path('new-communities/', views.communities_new, name="new-communities"),
     path('<slug:slug>', views.communities_page, name="page")
 ]
--- lab1/lab1/lab1/communities/views.py
+++ lab1/lab1/lab1/communities/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Communities
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 def communities(request):
     communities = Communities.objects.all().order_by('-date')
@@ -8,3 +10,16 @@ def communities(request):
 def communities_page(request, slug):
     communities = Communities.objects.get(slug=slug)
     return render(request, 'communities/communities_page.html', {'communities': communities})
+
+
+@login_required(login_url="/users/login/")
+def communities_new(request):
+    if request.method == 'POST': 
+        form = forms.CreatePost(request.POST, request.FILES) 
+        if form.is_valid():
+            newpost = form.save(commit=False) 
+            newpost.save()
+            return redirect('communities:communities')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'communities/communities_new.html', { 'form': form })
